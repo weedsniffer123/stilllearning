334b10de9ddde247b02e1e5c8d46d20c9f140f4b
Gading <gadingazmimarpaung@gmail.com>
2024-09-17 21:48:54 +0800

    initial commit

---
+++ belajargithub/code.py
@@ -0,0 +1,11 @@
+username = input ("Masukkan Username: ")
+
+
+if username == 'gaz':
+    password = int(input ("Masukkan Password: "))
+    if password == 123:
+        print("Selamat datang")
+    else:
+        print("Password salah")
+else:
+    print("Username tidak terdaftar")
